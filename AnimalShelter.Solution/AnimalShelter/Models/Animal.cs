91965caa1fae166acc3bd50c08d6260bd0d13560
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-26 11:56:48 -0700

    Added new views, models, and controllers
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- AnimalShelter.Solution/AnimalShelter/Models/Animal.cs
+++ AnimalShelter.Solution/AnimalShelter/Models/Animal.cs
@@ -1,9 +1,16 @@
 using System.Collections.Generic;
+using System;
 
 namespace AnimalShelter.Models
 {
   public class Animal
   {
-
+    public int AnimalId { get; set; }
+    public string Species { get; set; }
+    public string Breed { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public DateTime DateAdmit { get; set; } 
   }
 }
